c74591a6c1c91e46f3fcc1eb18091c55993bd5c9
Jerry Pussinen <jerry.pussinen@gmail.com>
2020-03-28 13:50:38 +0200

    Add 10 dummy functions and 10 dummy tests

--- my_project/some_functionality.py
+++ my_project/some_functionality.py
@@ -1,9 +1,45 @@
 import time
 
 
-def do_something():
-    time.sleep(10)
+def do_something1():
+    _do_work()
 
 
-def do_something_else():
-    time.sleep(10)
+def do_something2():
+    _do_work()
+
+
+def do_something3():
+    _do_work()
+
+
+def do_something4():
+    _do_work()
+
+
+def do_something5():
+    _do_work()
+
+
+def do_something6():
+    _do_work()
+
+
+def do_something7():
+    _do_work()
+
+
+def do_something8():
+    _do_work()
+
+
+def do_something9():
+    _do_work()
+
+
+def do_something10():
+    _do_work()
+
+
+def _do_work():
+    time.sleep(30)
--- tests/test_my_project.py
+++ tests/test_my_project.py
@@ -2,20 +2,40 @@ from my_project import some_functionality
 
 
 def test_1():
-    some_functionality.do_something()
+    some_functionality.do_something1()
 
 
 def test_2():
-    some_functionality.do_something()
+    some_functionality.do_something2()
 
 
 def test_3():
-    some_functionality.do_something()
+    some_functionality.do_something3()
 
 
 def test_4():
-    some_functionality.do_something_else()
+    some_functionality.do_something4()
 
 
 def test_5():
-    some_functionality.do_something_else()
+    some_functionality.do_something5()
+
+
+def test_6():
+    some_functionality.do_something6()
+
+
+def test_7():
+    some_functionality.do_something7()
+
+
+def test_8():
+    some_functionality.do_something8()
+
+
+def test_9():
+    some_functionality.do_something9()
+
+
+def test_10():
+    some_functionality.do_something10()
