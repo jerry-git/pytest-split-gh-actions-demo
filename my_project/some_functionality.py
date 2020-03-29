44177286943d2624001f165137126c666744d2e3
Jerry Pussinen <jerry.pussinen@gmail.com>
2020-03-29 12:59:11 +0300

    Add some variation to execution times

--- my_project/some_functionality.py
+++ my_project/some_functionality.py
@@ -2,44 +2,48 @@ import time
 
 
 def do_something1():
-    _do_work()
+    _do_work_light()
 
 
 def do_something2():
-    _do_work()
+    _do_work_light()
 
 
 def do_something3():
-    _do_work()
+    _do_work_light()
 
 
 def do_something4():
-    _do_work()
+    _do_work_light()
 
 
 def do_something5():
-    _do_work()
+    _do_work_light()
 
 
 def do_something6():
-    _do_work()
+    _do_work_light()
 
 
 def do_something7():
-    _do_work()
+    _do_work_heavy()
 
 
 def do_something8():
-    _do_work()
+    _do_work_heavy()
 
 
 def do_something9():
-    _do_work()
+    _do_work_heavy()
 
 
 def do_something10():
-    _do_work()
+    _do_work_heavy()
 
 
-def _do_work():
-    time.sleep(30)
+def _do_work_light():
+    time.sleep(10)
+
+
+def _do_work_heavy():
+    time.sleep(60)
